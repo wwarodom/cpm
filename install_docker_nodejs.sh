7d7903f3a4f2df8e9d676246fbcaba1a7edbaff8
root <root@example.localdomain>
2019-03-02 09:39:04 +0000

    Fix install docker node script

--- install_docker_nodejs.sh
+++ install_docker_nodejs.sh
@@ -1,21 +1,19 @@
 #=======================
 #  Install docker
 #======================
-apt update  \
-   &&  apt install --yes apt-transport-https ca-certificates curl gnupg2 software-properties-common \
-   && curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - \ 
-   && add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" \
-   && apt update \
-   && apt-cache policy docker-ce \
-   && apt install --yes docker-ce   \
-   && systemctl status docker
-
+sudo apt update  \
+   && sudo apt install --yes apt-transport-https ca-certificates curl gnupg2 software-properties-common \
+   && sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - \
+   && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" \
+   && sudo apt update \
+   && sudo apt-cache policy docker-ce \
+   && sudo apt install --yes docker-ce
 
 #========================
 # Install docker-compose
 #========================
-curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose \
-   && chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose \
+   && sudo chmod +x /usr/local/bin/docker-compose
 
 
 #======================
@@ -23,16 +21,15 @@ curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compos
 #======================
 curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh \
    && chmod 700 nodesource_setup.sh \
-   && ./nodesource_setup.sh \
-
-   && apt install --yes gcc g++ make  \
-   && curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -  \
-   && echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list \
-   && sudo apt-get update && sudo apt-get install yarn \
-   && apt install --yes build-essential \
-
-   && npm i -g pm2
+   && sudo ./nodesource_setup.sh \
+   && sudo apt install --yes gcc g++ make  \
+   && sudo curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -  \
+   && sudo echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list \
+   && sudo apt-get update && sudo apt install --yes yarn \
+   && sudo apt install --yes build-essential \
+   && sudo npm i -g pm2
 
 
 # Created by: Warodom Werapun
 # 2 Mar 2019
+
--- readme.md
+++ readme.md
@@ -1,9 +1,10 @@
 ### CPM: Caddy Php Mariadb
-CPM is a docker container source that provides Caddy web server and mariadb. It aims to use with Nodejs and Express server. 
-This script is recommended to use on Debian 9.4.
+CPM is a docker container source that provides Caddy web server and mariadb. It aims to use with Nodejs and Express server.
+This script is recommended to use on Debian 9.7 x64.
   - Pre-Requirements: docker docker-compose (provide on install_docker_node.sh script)
+  - run: `$ apt update && apt install git`
 
- 
+
 #### Steps
   - run: `$ git clone https://github.com/wwarodom/cpm.git`
   - run: `$ ./install_docker_node.sh`
